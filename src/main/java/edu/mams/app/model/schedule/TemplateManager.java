918bac53eb4143ac0cc6674834cb24a5eacbf2c5
coreylu2027u <cglu1@wpi.edu>
2026-01-14 15:41:47 -0500

    Move default schedule templates from `Tester` to `TemplateManager` for better organization and initialization.

--- src/main/java/edu/mams/app/model/schedule/TemplateManager.java
+++ src/main/java/edu/mams/app/model/schedule/TemplateManager.java
@@ -1,9 +1,15 @@
 package edu.mams.app.model.schedule;
 
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.*;
 
 public class TemplateManager {
     private static Map<String, DayTemplate> templates = new HashMap<>();
+
+    static {
+        loadDefaultTemplates();
+    }
 
     public static void addTemplate(DayTemplate template) {
         templates.put(template.getName(), template);
@@ -19,5 +25,66 @@ public class TemplateManager {
 
     public static List<String> listTemplateNames() {
         return new ArrayList<>(templates.keySet());
+    }
+
+    private static void loadDefaultTemplates() {
+        // Homeroom Day
+        TemplateManager.addTemplate(new DayTemplate(
+                "Class Meeting Day",
+                List.of(
+                        new BlockDefinition("ClassBlock", LocalTime.of(7,45), Duration.ofMinutes(60), "Block 1"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(8,45), Duration.ofMinutes(60), "Block 2"),
+                        new BlockDefinition("AllSchoolEvent", LocalTime.of(9,45), Duration.ofMinutes(30), "Class Meeting"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(10,15), Duration.ofMinutes(60), "Block 3"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(11,15), Duration.ofMinutes(60), "Block 4"),
+                        new BlockDefinition("AllSchoolEvent", LocalTime.of(12,15), Duration.ofMinutes(30), "Lunch"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(12,45), Duration.ofMinutes(60), "Block 5"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(13,45), Duration.ofMinutes(60), "Block 6")
+                )
+        ));
+
+        // Homeroom Day
+        TemplateManager.addTemplate(new DayTemplate(
+                "Homeroom Day",
+                List.of(
+                        new BlockDefinition("ClassBlock", LocalTime.of(7,45), Duration.ofMinutes(60), "Block 1"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(8,45), Duration.ofMinutes(60), "Block 2"),
+                        new BlockDefinition("AllSchoolEvent", LocalTime.of(9,45), Duration.ofMinutes(30), "Homeroom"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(10,15), Duration.ofMinutes(60), "Block 3"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(11,15), Duration.ofMinutes(60), "Block 4"),
+                        new BlockDefinition("AllSchoolEvent", LocalTime.of(12,15), Duration.ofMinutes(30), "Lunch"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(12,45), Duration.ofMinutes(60), "Block 5"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(13,45), Duration.ofMinutes(60), "Block 6")
+                )
+        ));
+
+        // Flex Day
+        TemplateManager.addTemplate(new DayTemplate(
+                "Flex Day",
+                List.of(
+                        new BlockDefinition("ClassBlock", LocalTime.of(7,45), Duration.ofMinutes(60), "Block 1"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(8,45), Duration.ofMinutes(60), "Block 2"),
+                        new BlockDefinition("AllSchoolEvent", LocalTime.of(9,45), Duration.ofMinutes(15), "15 min break"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(10,0), Duration.ofMinutes(60), "Block 3"),
+                        new BlockDefinition("AllSchoolEvent", LocalTime.of(11,0), Duration.ofMinutes(75), "Flex"),
+                        new BlockDefinition("AllSchoolEvent", LocalTime.of(12,15), Duration.ofMinutes(30), "Lunch"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(12,45), Duration.ofMinutes(60), "Block 4"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(13,45), Duration.ofMinutes(60), "Block 5")
+                )
+        ));
+
+        TemplateManager.addTemplate(new DayTemplate(
+                "PE Day",
+                List.of(
+                        new BlockDefinition("ClassBlock", LocalTime.of(7,45), Duration.ofMinutes(60), "Block 1"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(8,45), Duration.ofMinutes(60), "Block 2"),
+                        new BlockDefinition("AllSchoolEvent", LocalTime.of(9,45), Duration.ofMinutes(15), "15 min break"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(10,0), Duration.ofMinutes(60), "Block 3"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(11,0), Duration.ofMinutes(60), "Block 4"),
+                        new BlockDefinition("AllSchoolEvent", LocalTime.of(12,0), Duration.ofMinutes(30), "Lunch"),
+                        new BlockDefinition("ClassBlock", LocalTime.of(12,30), Duration.ofMinutes(60), "Block 5"),
+                        new BlockDefinition("AllSchoolEvent", LocalTime.of(13,30), Duration.ofMinutes(75), "PE")
+                )
+        ));
     }
 }
--- src/main/java/edu/mams/app/model/util/Tester.java
+++ src/main/java/edu/mams/app/model/util/Tester.java
@@ -51,65 +51,6 @@ public class Tester {
         ScheduleBuilder.setSplitClass((Course) classes.get(5));
         ScheduleBuilder.setSplitSection(sections.get(1));
 
-        // Homeroom Day
-        TemplateManager.addTemplate(new DayTemplate(
-                "Class Meeting Day",
-                List.of(
-                        new BlockDefinition("ClassBlock", LocalTime.of(7,45), Duration.ofMinutes(60), "Block 1"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(8,45), Duration.ofMinutes(60), "Block 2"),
-                        new BlockDefinition("AllSchoolEvent", LocalTime.of(9,45), Duration.ofMinutes(30), "Class Meeting"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(10,15), Duration.ofMinutes(60), "Block 3"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(11,15), Duration.ofMinutes(60), "Block 4"),
-                        new BlockDefinition("AllSchoolEvent", LocalTime.of(12,15), Duration.ofMinutes(30), "Lunch"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(12,45), Duration.ofMinutes(60), "Block 5"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(13,45), Duration.ofMinutes(60), "Block 6")
-                )
-        ));
-
-        // Homeroom Day
-        TemplateManager.addTemplate(new DayTemplate(
-                "Homeroom Day",
-                List.of(
-                        new BlockDefinition("ClassBlock", LocalTime.of(7,45), Duration.ofMinutes(60), "Block 1"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(8,45), Duration.ofMinutes(60), "Block 2"),
-                        new BlockDefinition("AllSchoolEvent", LocalTime.of(9,45), Duration.ofMinutes(30), "Homeroom"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(10,15), Duration.ofMinutes(60), "Block 3"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(11,15), Duration.ofMinutes(60), "Block 4"),
-                        new BlockDefinition("AllSchoolEvent", LocalTime.of(12,15), Duration.ofMinutes(30), "Lunch"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(12,45), Duration.ofMinutes(60), "Block 5"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(13,45), Duration.ofMinutes(60), "Block 6")
-                        )
-        ));
-
-        // Flex Day
-        TemplateManager.addTemplate(new DayTemplate(
-                "Flex Day",
-                List.of(
-                        new BlockDefinition("ClassBlock", LocalTime.of(7,45), Duration.ofMinutes(60), "Block 1"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(8,45), Duration.ofMinutes(60), "Block 2"),
-                        new BlockDefinition("AllSchoolEvent", LocalTime.of(9,45), Duration.ofMinutes(15), "15 min break"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(10,0), Duration.ofMinutes(60), "Block 3"),
-                        new BlockDefinition("AllSchoolEvent", LocalTime.of(11,0), Duration.ofMinutes(75), "Flex"),
-                        new BlockDefinition("AllSchoolEvent", LocalTime.of(12,15), Duration.ofMinutes(30), "Lunch"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(12,45), Duration.ofMinutes(60), "Block 4"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(13,45), Duration.ofMinutes(60), "Block 5")
-                )
-        ));
-
-        TemplateManager.addTemplate(new DayTemplate(
-                "PE Day",
-                List.of(
-                        new BlockDefinition("ClassBlock", LocalTime.of(7,45), Duration.ofMinutes(60), "Block 1"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(8,45), Duration.ofMinutes(60), "Block 2"),
-                        new BlockDefinition("AllSchoolEvent", LocalTime.of(9,45), Duration.ofMinutes(15), "15 min break"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(10,0), Duration.ofMinutes(60), "Block 3"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(11,0), Duration.ofMinutes(60), "Block 4"),
-                        new BlockDefinition("AllSchoolEvent", LocalTime.of(12,0), Duration.ofMinutes(30), "Lunch"),
-                        new BlockDefinition("ClassBlock", LocalTime.of(12,30), Duration.ofMinutes(60), "Block 5"),
-                        new BlockDefinition("AllSchoolEvent", LocalTime.of(13,30), Duration.ofMinutes(75), "PE")
-                )
-        ));
-
 
         Day monday = new Day(LocalDate.of(2025, 12, 1), 71, sections, classes);
         Day tuesday = new Day(LocalDate.of(2025, 12, 2), 72, sections, classes);
